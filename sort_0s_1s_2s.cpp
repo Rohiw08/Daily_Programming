0faa7aef15186601d3f9880ca4730f5ff522ce08
Rohit Waghmode <rohitwaghmode66@gmail.com>
2024-09-09 12:08:05 +0530

    sep 9 DPQ

---
+++ sort_0s_1s_2s.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    vector<int> map(3, 0);
+    for(int i : nums) map[i]++;
+    int j = 0;
+    for(int i = 0; i <= 2; i++){
+        int count = map[i];
+        while(count--) nums[j++] = i;
+    }
+}
+
+void printa(auto a){
+    for(auto i : a){
+        cout<<i<<" ";
+    }
+}
+
+int main(){
+    vector<int> question = {0, 1, 2, 1, 0, 2, 1, 0};
+    sortColors(question);
+    printa(question);
+    return 0;
+}
