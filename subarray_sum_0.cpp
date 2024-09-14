2592c1748a844b6a625ba47fc184872b0ae30c68
Rohit Waghmode <rohitwaghmode66@gmail.com>
2024-09-14 11:30:46 +0530

    14 sep DPQ

--- subarray_sum_0.cpp
+++ subarray_sum_0.cpp
@@ -8,14 +8,11 @@ vector<vector<int>> subArraySum(vector<int> &nums){
     vector<vector<int>> ans;
     int prefix = 0;
 
-    // map[0].push_back(-1);
+    map[0].push_back(-1);
 
     int n = nums.size();
     for(int i = 0; i < n; i++){
         prefix += nums[i];
-        
-        if(prefix == 0) 
-            ans.push_back({0, i});
 
         if(map.find(prefix) != map.end())
             for(int inx : map[prefix])
