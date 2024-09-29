6abf468d802cb50be516cb94d84318cdd29ec9ef
Rohit Waghmode <rohitwaghmode66@gmail.com>
2024-09-29 23:40:04 +0530

    29 sep DPQ

--- reverse_stack.cpp
+++ reverse_stack.cpp
@@ -3,38 +3,26 @@
 #include<string>
 using namespace std;
 
-void insert(stack<int> &s, int temp) {
-    if(s.empty() || s.top() >= temp) {
+void insert(stack<int> &s, int temp){
+    if(s.empty()){
         s.push(temp);
         return;
     }
-
     int curr = s.top();
     s.pop();
     insert(s, temp);
     s.push(curr);
 }
 
-void sort(stack<int> &s) {
-    if(s.size() == 1 || s.empty()) 
-        return;
-
+void reverseStack(stack<int> &s){
+    if(s.empty()) return;
     int temp = s.top();
     s.pop();
-    sort(s);
+    reverseStack(s);
     insert(s, temp);
 }
 
-void print_stack(stack<int> s) {
-    cout << "[";
-    while(!s.empty()) {
-        cout << s.top() << " ";
-        s.pop();
-    }
-    cout << "]" << endl;
-}
-
-void push_stack(stack<int> &s) {
+void pushStack(stack<int> &s) {
     while(true) {
         cout << "Type an integer value to push (type 'c' to stop): " << endl;
         string val = "";
@@ -48,11 +36,9 @@ void push_stack(stack<int> &s) {
     }
 }
 
-int main() {
+int main(){
     stack<int> question;
-    push_stack(question);
-    sort(question);
-    print_stack(question);
-    
+    pushStack(question);
+    reverseStack(question);
     return 0;
 }
